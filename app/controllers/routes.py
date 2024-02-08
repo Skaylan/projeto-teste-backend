a9b80a3ee20cf712bc07cf22ebb5ef85eaf2eff4
Lucas Oliveira <lucas_7tv@hotmail.com>
2024-02-08 17:21:12 -0300

    feat: mudanças nas rotas e nas tabelas

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -8,7 +8,7 @@ from app.models.tables.user import User
 from app.models.tables.user_type import UserType
 from app.models.schemas.user_schema import UserSchema
 from app.models.schemas.user_type_schema import UserTypeSchema
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
         
 @app.route('/api/create_user', methods=['POST'])
@@ -23,7 +23,7 @@ def create_user():
             
             password_hash = generate_password_hash(password)
             
-            user = User(name=name, email=email, password_hash=password_hash, user_type=user_type_id)
+            user = User(name=name, email=email, password_hash=password_hash, user_type_id=user_type_id)
             db.session.add(user)
             db.session.commit()
             
@@ -107,6 +107,51 @@ def get_user_type():
                 'users': payload
             }), 200
             
+        except Exception as error:
+            print(f'error class: {error.__class__} | error cause: {error.__cause__}')
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+            return jsonify({
+                    'status': 'error',
+                    'message': 'An error has occurred!',
+                    'error_class': str(error.__class__),
+                    'error_cause': str(error.__cause__)
+                }), 500
+
+
+@app.route('/api/authenticate_user', methods=['POST'])
+def authenticate_user():
+    if request.method == 'POST':
+        try:
+            body = request.get_json()
+            email = body.get('email')
+            password = body.get('password')
+            
+            user = User.query.filter_by(email=email).first()
+            
+            if user == None:
+                return jsonify({
+                    'status': 'error',
+                    'message': 'user not found!'
+                }), 200
+                
+            checked_password = check_password_hash(user.password_hash, password)
+            
+            if checked_password: 
+                schema = UserSchema()
+                payload = schema.dump(user)
+                
+                return jsonify({
+                    'user': payload,
+                    'status': 'ok'
+                }), 200
+            else:
+                return jsonify({
+                    'status': 'error',
+                    'message': 'Senha incorreta!'
+                }), 200
+                
         except Exception as error:
             print(f'error class: {error.__class__} | error cause: {error.__cause__}')
             exc_type, exc_obj, exc_tb = sys.exc_info()
--- app/models/tables/user_type.py
+++ app/models/tables/user_type.py
@@ -1,6 +1,6 @@
 from app.config.app_config import *
 from app.config.db_config import *
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship, backref
 from sqlalchemy import String, Text
 from uuid import uuid4, UUID
 from datetime import datetime
@@ -9,8 +9,7 @@ from datetime import datetime
 class UserType(Base):
     id = db.Column(String(36), primary_key=True, default=lambda: str(uuid4()))
     descr: Mapped[str] = mapped_column(String(45), unique=False, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    owner_id = relationship('User', backref='user_type')
     
     
     def __init__(self, descr: str):
